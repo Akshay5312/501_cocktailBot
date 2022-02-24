d5deb98ffc5d99ac0157205a650da2767c2b3741
Akshay5312 <40531389+Akshay5312@users.noreply.github.com>
2022-02-24 15:45:14 -0500

    All fixed!

--- src/KinematicsTester.m
+++ src/KinematicsTester.m
@@ -3,11 +3,11 @@ Q = [0 0 0 0 0 0];
 %K.getFK([0 0 0])
 
 Fr = K.getFrames(Q)
-LinkPoints = zeros(7,3)
+LinkPoints = zeros(3,7)
 
 for m = 1:7
     Frame = Fr{m}
-    LinkPoints(m,:) = Frame(1:3,4)'
+    LinkPoints(:,m) = Frame(1:3,4)
 end
 
 
--- src/PlotSim.m
+++ src/PlotSim.m
@@ -19,13 +19,16 @@ classdef PlotSim
             %X = [LinkPoints(1,1:2); LinkPoints(1,2:3);... Linkpoints(1,n-1:n)
             %Y = [LinkPoints(2,1:2); LinkPoints(2,2:3);... Linkpoints(2,n-1:n)
             %Z = [LinkPoints(3,1:2); LinkPoints(3,2:3);... Linkpoints(3,n-1:n)
-            for n = 2:size(2,LinkPoints)
+            for n = 2:size(LinkPoints,2)
                 X = [X;LinkPoints( 1, n-1:n)];
                 Y = [Y;LinkPoints( 2, n-1:n)];
                 Z = [Z;LinkPoints( 3, n-1:n)];
             end
 
             plot3(X,Y,Z)
+            hold on
+            plot3(X,Y,Z, 'o')
+            hold off
             drawnow
         end
         
