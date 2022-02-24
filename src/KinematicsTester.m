8e7e7e4e6ef4243626aec084cff95879e14e4f59
Akshay5312 <40531389+Akshay5312@users.noreply.github.com>
2022-02-24 15:27:53 -0500

    Finished FK
    Plotter isnt working perfectly, something wrong with arbitrary dimensions

--- src/KinematicsTester.m
+++ src/KinematicsTester.m
@@ -1,3 +1,17 @@
-K = BotKinematics()
+K = BotKinematics();
+Q = [0 0 0 0 0 0];
+%K.getFK([0 0 0])
 
-K.getFK([0 0 0])
+Fr = K.getFrames(Q)
+LinkPoints = zeros(7,3)
+
+for m = 1:7
+    Frame = Fr{m}
+    LinkPoints(m,:) = Frame(1:3,4)'
+end
+
+
+
+ps = PlotSim();
+
+ps.plot(LinkPoints)
